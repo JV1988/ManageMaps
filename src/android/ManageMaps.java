ede3fb0505770f317b6c4dca83a37904a3f3511b
JV1988 <jeroen@periskal.com>
2014-01-30 10:06:04 +0100

    Update ManageMaps.java

--- src/android/ManageMaps.java
+++ src/android/ManageMaps.java
@@ -13,24 +13,24 @@ public class ManageMaps extends CordovaPlugin {
     public static final String ACTION_GO_TO_LAT_LON_POSITION = "goToLatLonPosition";
     
     @Override
-    public JSONObject execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
             if (ACTION_GO_TO_LAT_LON_POSITION.equals(action)) { 
                 JSONObject arg_object = args.getJSONObject(0);
                 
                 Tile test = new Tile(arg_object.getFloat("latitude"), arg_object.getFloat("longitude"));
-                JSONObject object=new JSONObject();
-		object.put("newRow",test);
+                //JSONObject object=new JSONObject();
+		//object.put("newRow",test);
                 
                 callbackContext.success();
-                return object;
+                return true;
             }
             callbackContext.error("Invalid action");
             return false;
         } catch(Exception e) {
             System.err.println("Exception: " + e.getMessage());
             callbackContext.error(e.getMessage());
-            return null;
+            return false;
         } 
     }
 }
