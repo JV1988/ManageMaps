ff69b0a677776e1447e8c25e5243ac1eafe5e764
JV1988 <jeroen@periskal.com>
2014-01-30 09:55:35 +0100

    Update ManageMaps.java

--- src/android/ManageMaps.java
+++ src/android/ManageMaps.java
@@ -10,31 +10,27 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class ManageMaps extends CordovaPlugin {
-    public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
+    public static final String ACTION_GO_TO_LAT_LON_POSITION = "goToLatLonPosition";
     
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public JSONObject execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
-            if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) { 
+            if (ACTION_GO_TO_LAT_LON_POSITION.equals(action)) { 
                 JSONObject arg_object = args.getJSONObject(0);
-                Intent calIntent = new Intent(Intent.ACTION_EDIT)
-                    .setType("vnd.android.cursor.item/event")
-                    .putExtra("beginTime", arg_object.getLong("startTimeMillis"))
-                    .putExtra("endTime", arg_object.getLong("endTimeMillis"))
-                    .putExtra("title", arg_object.getString("title"))
-                    .putExtra("description", arg_object.getString("description"))
-                    .putExtra("eventLocation", arg_object.getString("eventLocation"));
-             
-               this.cordova.getActivity().startActivity(calIntent);
+                
+                Tile test = new Tile(arg_object.getFloat("latitude"), arg_object.getFloat("longitude"));
+                JSONObject object=new JSONObject();
+				
+                
                callbackContext.success();
-               return true;
+               return object;
             }
             callbackContext.error("Invalid action");
             return false;
         } catch(Exception e) {
             System.err.println("Exception: " + e.getMessage());
             callbackContext.error(e.getMessage());
-            return false;
+            return null;
         } 
     }
 }
