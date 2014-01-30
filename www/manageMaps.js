24bf0404b2c4698863f9bed208c6553798d47560
JV1988 <jeroen@periskal.com>
2014-01-30 09:58:04 +0100

    Update manageMaps.js

--- www/manageMaps.js
+++ www/manageMaps.js
@@ -1,16 +1,13 @@
 var manageMaps =  {
-    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    goToLatLonPosition: function(latitude, longitude, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'ManageMaps', // mapped to our native Java class called "Calendar"
-            'addCalendarEntry', // with this action name
+            'goToLatLonPosition', // with this action name
             [{                  // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
+                "latitude": latitude,
+                "longitude": notes
             }]
         );
     }
